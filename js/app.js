68425330cb53d1dcc9a4887e640714ee1a9bc522
Samer Alimoor <sipar.alamour@gmail.com>
2022-07-16 18:19:15 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var typing = new Typed(".typing", {
+    strings: ["Software Developer", "Computer Teacher", "Database Supervisor", "IT Solution"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
+ 
+$("html").niceScroll({
+   
+    cursorcolor:"#ec1839",
+    
+    smoothscroll:true,
+    sensitiverail:true,
+    cursorfixedheight:false,
+    enablemouselockapi:true,
+    hidecursordelay: 400,
+   
+    horizrailenabled:false
+
+    });
+    
